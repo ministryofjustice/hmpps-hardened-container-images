b3c6e4206413124eb8319c318a79c92b54e28234
charliesmith-justice <charlie.smith@justice.gov.uk>
2026-08-12 09:48:57 +0100

    feat: new DevSecOps workflow
    Signed-off-by: charliesmith-justice <charlie.smith@justice.gov.uk>

--- clean-up-packages.sh
+++ clean-up-packages.sh
@@ -16,41 +16,45 @@ for package in ${packages}; do
   [[ "${package}" != hmpps-hardened-* ]] && continue
 
   echo
-  echo "Processing ${package}..."
+  echo "Processing / Deleting ${package}..."
+  if [[ "${DRY_RUN}" == "false" ]]; then
+    gh api \
+      -X DELETE \
+      "/orgs/${ORG}/packages/container/${package}" || true
+  fi
+  # gh api \
+  #   "/orgs/${ORG}/packages/container/${package}/versions" \
+  #   --paginate |
+  #   jq -c '.[]' |
+  #   while read -r version; do
 
-  gh api \
-    "/orgs/${ORG}/packages/container/${package}/versions" \
-    --paginate |
-    jq -c '.[]' |
-    while read -r version; do
+  #     version_id=$(echo "${version}" | jq -r '.id')
 
-      version_id=$(echo "${version}" | jq -r '.id')
+  #     keep=false
 
-      keep=false
+  #     while read -r tag; do
+  #       [[ -z "${tag}" ]] && continue
 
-      while read -r tag; do
-        [[ -z "${tag}" ]] && continue
+  #       # Keep release tags such as v1.0.0
+  #       if [[ "${tag}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  #         keep=true
+  #       fi
+  #     done < <(
+  #       echo "${version}" | jq -r '.metadata.container.tags[]?'
+  #     )
 
-        # Keep release tags such as v1.0.0
-        if [[ "${tag}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-          keep=true
-        fi
-      done < <(
-        echo "${version}" | jq -r '.metadata.container.tags[]?'
-      )
+  #     if [[ "${keep}" == "false" ]]; then
+  #       tags=$(echo "${version}" | jq -c '.metadata.container.tags // []')
 
-      if [[ "${keep}" == "false" ]]; then
-        tags=$(echo "${version}" | jq -c '.metadata.container.tags // []')
+  #       echo "DELETE ${package}"
+  #       echo "  Version ID: ${version_id}"
+  #       echo "  Tags: ${tags}"
 
-        echo "DELETE ${package}"
-        echo "  Version ID: ${version_id}"
-        echo "  Tags: ${tags}"
-
-        if [[ "${DRY_RUN}" == "false" ]]; then
-          gh api \
-            -X DELETE \
-            "/orgs/${ORG}/packages/container/${package}/versions/${version_id}" || true
-        fi
-      fi
-    done
+  #       if [[ "${DRY_RUN}" == "false" ]]; then
+  #         gh api \
+  #           -X DELETE \
+  #           "/orgs/${ORG}/packages/container/${package}/versions/${version_id}" || true
+  #       fi
+  #     fi
+  #   done
 done
